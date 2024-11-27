3148a8e5805c199205b90f5cc7396ae9a1c8de59
Raihan <aboraihan14@gmail.com>
2024-11-27 17:14:03 +0600

    changes in route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,7 +12,7 @@ export const router = createBrowserRouter([
         element: <MainLayout />,
         children: [
             {
-                path: "/l",
+                path: "/",
                 element: <L />
             },
             {
